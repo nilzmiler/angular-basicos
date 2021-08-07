90272c48e72a6fd587c83fd209d6d884e5695d0b
ctohasa <ctohasa@cobiscorp.com>
2021-08-06 19:46:21 -0500

    Final de la seccion 6

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -13,15 +13,12 @@ export class AgregarComponent {
     poder: 0
   }
 
-  // @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
-
   constructor( private dbzService: DbzService ) {}
 
   agregar() {
 
     if( this.nuevo.nombre.trim().length == 0) { return; }
 
-    // this.onNuevoPersonaje.emit( this.nuevo );
     this.dbzService.agregarPersonaje( this.nuevo );
 
     this.nuevo = {
